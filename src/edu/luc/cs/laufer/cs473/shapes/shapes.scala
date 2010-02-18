e1eb4e61c1f6a45fc7b67ab25dcd66d95fed7ba8
klaeufer <klaeufer@6eb1746a-ae27-11de-a8b3-6bed2ae3e0d9>
2010-02-18 19:22:07 +0000

    Some minor changes (added validity checking on constructor arguments)

--- src/edu/luc/cs/laufer/cs473/shapes/shapes.scala
+++ src/edu/luc/cs/laufer/cs473/shapes/shapes.scala
@@ -1,6 +1,14 @@
 package edu.luc.cs.laufer.cs473.shapes
 
 abstract class Shape
+
 case class Rectangle(width: Int, height: Int) extends Shape
-case class Location(x: Int, y: Int, shape: Shape) extends Shape
+
+case class Location(x: Int, y: Int, shape: Shape) extends Shape {
+  if (shape == null) {
+    throw new IllegalArgumentException("null shape in location")
+  }
+}
+
 // TODO add missing case classes (see test fixtures)
+// TODO must include validity checking for constructor arguments
