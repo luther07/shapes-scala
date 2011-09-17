3bed089e6f4c1425e64704b90be21fdba0e2e965
Mark Johnson <luther07@gmail.com>
2011-09-17 14:51:51 -0400

    defined first couple case classes

--- src/edu/luc/cs/laufer/cs473/shapes/shapes.scala
+++ src/edu/luc/cs/laufer/cs473/shapes/shapes.scala
@@ -10,5 +10,13 @@ case class Location(x: Int, y: Int, shape: Shape) extends Shape {
   }
 }
 
+case class Ellipse(foci_distance_sum: Int, foci_line_length: Int) extends Shape {
+  if (foci_distance_sum < foci_line_length) {
+    throw new IllegalArgumentException("invalid ellipse: foci_distance_sum must be greater than foci_line_length")
+  }
+}
+
+case class Group(shape1: Shape, shape2: Shape) extends Shape
+
 // TODO add missing case classes (see test fixtures)
 // TODO must include validity checking for constructor arguments
